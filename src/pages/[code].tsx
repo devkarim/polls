a9345f6cea8b2fd9fb4127f279929fd4692dd077
devkarim <devkarim@hotmail.com>
2022-10-17 19:26:40 +0200

    setup poll details page

--- src/pages/[code].tsx
+++ src/pages/[code].tsx
@@ -1,13 +1,29 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import Loading from '../components/ui/Loading';
+import { trpc } from '../services/api/trpc';
 
 const HomePage: NextPage = () => {
   const router = useRouter();
-  const { code } = router.query;
+  const { code } = router.query as { code: string };
+  const { data, isLoading } = trpc.useQuery(['get-poll', { code }], {
+    refetchInterval: false,
+    refetchOnReconnect: false,
+    refetchOnWindowFocus: false,
+  });
+
+  if (isLoading) return <Loading />;
+
+  if (!data)
+    return (
+      <div className="min-h-screen md:h-screen md:min-h-0 flex flex-col items-center justify-center p-8">
+        <h2>404 - Poll Not Found</h2>
+      </div>
+    );
 
   return (
     <div className="min-h-screen md:h-screen md:min-h-0 flex flex-col items-center justify-center p-8">
-      Hi {code}
+      Hi {data?.code}
     </div>
   );
 };
